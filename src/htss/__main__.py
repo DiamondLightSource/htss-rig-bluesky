ba52120de048516845a37306f1f07670cb420a90
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-03-17 11:40:39 +0000

    Allow access to version via CLI

--- src/htss/__main__.py
+++ src/htss/__main__.py
@@ -1,8 +1,25 @@
+import importlib
+
+import click
 import IPython
 from traitlets.config import get_config
 
-from .startup import *  # noqa: F401, F403
+from htss import __version__
 
-conf = get_config()
-conf.InteractiveShellEmbed.colors = "Linux"
-IPython.embed(config=conf)
+
+@click.group(invoke_without_command=True)
+@click.version_option(version=__version__, prog_name="htss")
+@click.pass_context
+def main(ctx):
+    # if no command is supplied, run with the options passed
+    if ctx.invoked_subcommand is None:
+        mod = importlib.import_module("htss.startup")
+        globals().update(mod)
+
+        conf = get_config()
+        conf.InteractiveShellEmbed.colors = "Linux"
+        IPython.embed(config=conf)
+
+
+if __name__ == "__main__":
+    main()
