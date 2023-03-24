f6035f337145ae3cd319f470306eab9c181df9d7
Callum Forrester <29771545+callumforrester@users.noreply.github.com>
2023-03-24 10:40:01 +0000

    Container and ci (#4)
    * Ignore startup script from pytest
    
    * Allow access to version via CLI
    
    * Fix dockerfile
    
    * Fix module updating
    
    * Exclude dodal from pip freeze
    
    * Change CLI name to htss

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,4 +6,6 @@ from htss import __version__
 
 def test_cli_version():
     cmd = [sys.executable, "-m", "htss", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
+    assert (
+        subprocess.check_output(cmd).decode().strip() == f"htss, version {__version__}"
+    )
