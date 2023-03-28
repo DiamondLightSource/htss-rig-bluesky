f6e52c5ffb761226b3ae51482dd5bc4e63ee4d1d
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-03-28 09:01:37 +0100

    Do not evaluate beamline prefix until devices constructed

--- src/htss/devices.py
+++ src/htss/devices.py
@@ -3,11 +3,7 @@ import os
 from dodal.devices.areadetector import AdAravisDetector
 from ophyd import Component, EpicsMotor, MotorBundle
 
-BEAMLINE = os.environ.get("BEAMLINE")
-_PREFIXES = {"p46": "BL46P", "p47": "BL47P", "p48": "BL46P", "p49": "BL49P"}
-if BEAMLINE not in _PREFIXES.keys():
-    raise KeyError(f"Please set $BEAMLINE to one of {list(_PREFIXES.keys())}")
-PREFIX = _PREFIXES[BEAMLINE.lower()]
+BEAMLINE = os.environ.get("BEAMLINE", "")
 
 
 class SampleStage(MotorBundle):
@@ -16,11 +12,19 @@ class SampleStage(MotorBundle):
 
 
 def sample(name: str = "sample_stage") -> SampleStage:
-    return SampleStage(name=name, prefix=f"{PREFIX}-MO-MAP-01:STAGE:")
+    return SampleStage(name=name, prefix=f"{_prefix()}-MO-MAP-01:STAGE:")
 
 
 def det(name: str = "det") -> AdAravisDetector:
-    det = AdAravisDetector(name=name, prefix=f"{PREFIX}-EA-DET-01:")
+    det = AdAravisDetector(name=name, prefix=f"{_prefix()}-EA-DET-01:")
     det.hdf.reg_root = "/scratch/mybeamline/data"
     det.hdf.write_path_template = "%Y"
     return det
+
+
+def _prefix() -> str:
+    prefixes = {"p46": "BL46P", "p47": "BL47P", "p48": "BL46P", "p49": "BL49P"}
+    beamline = BEAMLINE.lower()
+    if beamline not in prefixes.keys():
+        raise KeyError(f"Please set $BEAMLINE to one of {list(prefixes.keys())}")
+    return prefixes[beamline]
