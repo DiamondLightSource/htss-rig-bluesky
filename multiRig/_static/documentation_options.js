b9882bceb54d6ce330979899e5ee93ea77527cbd
callumforrester <callumforrester@users.noreply.github.com>
2023-03-28 08:14:04 +0000

    deploy: d179c9e76b14e06775776c85fd60515a4adb0f07

--- multiRig/_static/documentation_options.js
+++ multiRig/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev58+gf6e52c5',
+    VERSION: '0.1.dev59+gd179c9e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
