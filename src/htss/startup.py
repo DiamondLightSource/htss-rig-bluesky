f20c514079cebeaa138cb058fad362bc4d5675e8
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-03-31 13:16:58 +0000

    Repress pyepics error messages

--- src/htss/startup.py
+++ src/htss/startup.py
@@ -2,6 +2,7 @@ import os
 
 import bluesky.plan_stubs as bps  # noqa: F401
 import bluesky.plans as bp  # noqa: F401
+import epics
 import matplotlib
 import matplotlib.pyplot as plt  # noqa: F401
 from bluesky import RunEngine
@@ -9,6 +10,7 @@ from bluesky.callbacks.best_effort import BestEffortCallback
 from dodal.utils import make_all_devices
 
 import htss.devices as devices
+from htss.plans.detector import Roi, ensure_detector_ready, set_roi  # noqa: F401
 from htss.plans.excercise import (  # noqa: F401
     excercise_beamline,
     excercise_detector,
@@ -34,6 +36,13 @@ else:
     tiled = get_client()
     tiled.login(username="htss")
     RE.subscribe(tiled.post_document)
+
+
+def handle_messages(text):
+    ...
+
+
+epics.ca.replace_printf_handler(handle_messages)
 
 startup_message = f"Startup completed, Bluesky in control of {BEAMLINE}"
 bar = "=" * len(startup_message)
