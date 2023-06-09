acb3224a9f023512a44f4b6d82201c52e3c7823f
callumforrester <callumforrester@users.noreply.github.com>
2023-06-09 08:44:24 +0000

    deploy: f500ac94fdd26e4be33217a46091f54e07dd6e17

--- tomo2/_static/documentation_options.js
+++ tomo2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev87+g29e102c',
+    VERSION: '0.1.dev98+gf500ac9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
