9459cd875fd340bdc2242cfa2b95823d3d28410c
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-07-13 13:49:40 +0000

    WIP on reconstruction

--- src/htss/processing/tomography.py
+++ src/htss/processing/tomography.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import numpy as np
 from xarray import Dataset
 
@@ -42,3 +43,31 @@ def normalize_tomography_data(
             "normalized_image": (["time", "dim_1", "dim_2"], normalized_images),
         }
     )
+
+
+def to_sinograms(
+    normalized_projections: Dataset,
+    image_label: str = "normalized_images",
+    linear_position_label: str = "sample_stage_x",
+) -> Dataset:
+    ...
+
+
+def determine_center_of_rotation(
+    normalized_projections: Dataset,
+    initial_guess: Optional[float] = None,
+    image_label: str = "normalized_images",
+    linear_position_label: str = "sample_stage_x",
+) -> float:
+    if initial_guess is None:
+        images = normalized_projections[image_label]
+        initial_guess = images.shape[2] / 2
+    center_of_rotation = initial_guess
+    linear_positions = np.array(normalized_projections[linear_position_label])
+    for ind, x in enumerate(linear_positions):
+        if x >= center_of_rotation and linear_positions[ind - 1] <= center_of_rotation:
+            center_of_rotation = (ind - 1) + (
+                center_of_rotation - linear_positions[ind - 1]
+            ) / float(x - linear_positions[ind - 1])
+            break
+    return center_of_rotation
