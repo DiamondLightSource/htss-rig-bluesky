b9e983abd3e26419ffac66456921d0ace01dc2f7
Zoheb Shaikh <zoheb.shaikh@diamond.ac.uk>
2024-12-19 12:01:45 +0000

    added system-test

---
+++ tests/system_tests/test_plans.py
@@ -0,0 +1,75 @@
+import pytest
+from blueapi.client.client import BlueapiClient
+from blueapi.client.event_bus import AnyEvent
+from blueapi.core.bluesky_types import DataEvent
+from blueapi.worker.event import TaskStatus, WorkerEvent, WorkerState
+from blueapi.worker.task import Task
+
+# Please export BEAMLINE=pXX before running the tests or add it in pyproject.toml
+
+
+def _check_all_events(all_events: list[AnyEvent]):
+    assert (
+        isinstance(all_events[0], WorkerEvent) and all_events[0].task_status is not None
+    )
+    task_id = all_events[0].task_status.task_id
+    # First event is WorkerEvent
+    assert all_events[0] == WorkerEvent(
+        state=WorkerState.RUNNING,
+        task_status=TaskStatus(
+            task_id=task_id,
+            task_complete=False,
+            task_failed=False,
+        ),
+    )
+
+    assert all(
+        isinstance(event, DataEvent) for event in all_events[1:-2]
+    ), "Middle elements must be DataEvents."
+
+    # Last 2 events are WorkerEvent
+    assert all_events[-2:] == [
+        WorkerEvent(
+            state=WorkerState.IDLE,
+            task_status=TaskStatus(
+                task_id=task_id,
+                task_complete=False,
+                task_failed=False,
+            ),
+        ),
+        WorkerEvent(
+            state=WorkerState.IDLE,
+            task_status=TaskStatus(
+                task_id=task_id,
+                task_complete=True,
+                task_failed=False,
+            ),
+        ),
+    ]
+
+
+@pytest.mark.parametrize("device", ["sample_stage", "panda", "det"])
+def test_device_present(client: BlueapiClient, device: str):
+    assert client.get_device(device), f"{device} is not available"
+
+
+@pytest.mark.parametrize(
+    "plan", ["step_scan_plan", "fly_and_collect_plan", "log_scan_plan"]
+)
+def test_spec_scan_task(
+    client: BlueapiClient,
+    task_definition: dict[str, Task],
+    plan: str,
+):
+    assert client.get_plan(plan), f"In {plan} is available"
+
+    all_events: list[AnyEvent] = []
+
+    def on_event(event: AnyEvent):
+        all_events.append(event)
+
+    client.run_task(task_definition[plan], on_event=on_event)
+
+    _check_all_events(all_events)
+
+    assert client.get_state() is WorkerState.IDLE
