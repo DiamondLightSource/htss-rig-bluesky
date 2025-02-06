336cdd70a752e3ced5ce24d5d1d3f2192d451e2f
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-02-06 11:34:22 +0000

    Create PWM loop to manage backlight

--- src/htss_rig_bluesky/plans/backlight.py
+++ src/htss_rig_bluesky/plans/backlight.py
@@ -1,6 +1,6 @@
 import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
-from ophyd_async.fastcs.panda import HDFPanda
+from ophyd_async.fastcs.panda import HDFPanda, TimeUnits
 
 
 def set_backlight_intensity(
@@ -10,17 +10,60 @@ def set_backlight_intensity(
     group: str | None = None,
 ) -> MsgGenerator[None]:
     if 0.0 <= intensity <= 1.0:
-        duty_cycle = int(round(intensity * 100.0))
+        group = group or set_backlight_intensity.__name__
+        duty_cycle = int(round(intensity * 100))
+
         if duty_cycle == 0:
             port = "ZERO"
         elif duty_cycle == 100:
             port = "ONE"
         else:
-            port = yield from create_pwm(panda, duty_cycle, wait, group)
+            port = yield from create_pwm(panda, intensity, wait=False, group=group)
 
-        yield from set_backlight_control_port(panda, port, wait, group)
+        yield from set_backlight_control_port(panda, port, wait=False, group=group)
+
+        if wait:
+            yield from bps.wait(group=group)
     else:
         raise ValueError(f"Given intensity {intensity} should be between 0.0 and 1.0")
+
+
+def create_pwm(
+    panda: HDFPanda,
+    duty_cycle: float,
+    period: float = 100.0,
+    wait: bool = True,
+    group: str | None = None,
+) -> MsgGenerator[str]:
+    live_time = period * duty_cycle
+    dead_time = period - live_time
+
+    clock = panda.clock[2]
+    pulse = panda.pulse[2]
+
+    group = group or create_pwm.__name__
+
+    for signal, value in {
+        # Set units first
+        clock.period_units: TimeUnits.US,
+        pulse.delay_units: TimeUnits.US,
+        pulse.width_units: TimeUnits.US,
+        # Then set all other values
+        clock.enable: "ONE",
+        clock.enable_delay: 0.0,
+        clock.period: period,
+        pulse.enable: "ONE",
+        pulse.trig: "CLOCK2.OUT",
+        pulse.delay: dead_time,
+        pulse.width: live_time,
+        pulse.trig_edge: "Rising",
+    }.items():
+        yield from bps.abs_set(signal, value, wait=False, group=group)
+
+    if wait:
+        yield from bps.wait(group)
+
+    return "PULSE2.OUT"
 
 
 def set_backlight_control_port(
@@ -29,19 +72,10 @@ def set_backlight_control_port(
     wait: bool = True,
     group: str | None = None,
 ) -> MsgGenerator[None]:
-    ttl_out_2 = panda.ttlout[2]
+    output_port = panda.ttlout[2]
     yield from bps.abs_set(
-        ttl_out_2.val,
+        output_port.val,
         port,
         wait=wait,
         group=group,
     )
-
-
-def create_pwm(
-    panda: HDFPanda,
-    duty_cycle: int,
-    wait: bool = True,
-    group: str | None = None,
-) -> MsgGenerator[str]:
-    return NotImplemented
