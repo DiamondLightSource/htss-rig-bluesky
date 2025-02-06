cc05d7b556e41b0816431be8090a9ee11168a434
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-02-06 14:02:03 +0000

    Fix conftest

--- tests/system_tests/conftest.py
+++ tests/system_tests/conftest.py
@@ -1,28 +1,9 @@
 import pytest
 from blueapi.client.client import BlueapiClient
 from blueapi.config import ApplicationConfig, RestConfig, StompConfig
-from blueapi.worker.task import Task
 from bluesky_stomp.models import BasicAuthentication
 
 from htss_rig_bluesky.names import BEAMLINE
-
-
-@pytest.fixture
-def task_definition() -> dict[str, Task]:
-    return {
-        "step_scan_plan": Task(
-            name="step_scan_plan",
-            params={"detectors": "det", "motor": "sample_stage.theta"},
-        ),
-        "fly_and_collect_plan": Task(
-            name="fly_and_collect_plan",
-            params={"panda": "panda", "diff": "det"},
-        ),
-        "log_scan_plan": Task(
-            name="log_scan_plan",
-            params={"detectors": "det", "motor": "sample_stage.x"},
-        ),
-    }
 
 
 @pytest.fixture
