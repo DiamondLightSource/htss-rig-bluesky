b53c05246eb3e83de0580feb77aac5f7eced4336
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-02-06 14:50:04 +0000

    Remove unused function

--- tests/system_tests/test_plans.py
+++ tests/system_tests/test_plans.py
@@ -10,46 +10,6 @@ from blueapi.worker.task import Task
 # Please export BEAMLINE=pXX before running the tests or add it in pyproject.toml
 
 
-def _check_all_events(all_events: list[AnyEvent]):
-    assert (
-        isinstance(all_events[0], WorkerEvent) and all_events[0].task_status is not None
-    )
-    task_id = all_events[0].task_status.task_id
-    # First event is WorkerEvent
-    assert all_events[0] == WorkerEvent(
-        state=WorkerState.RUNNING,
-        task_status=TaskStatus(
-            task_id=task_id,
-            task_complete=False,
-            task_failed=False,
-        ),
-    )
-
-    assert all(isinstance(event, DataEvent) for event in all_events[1:-2]), (
-        "Middle elements must be DataEvents."
-    )
-
-    # Last 2 events are WorkerEvent
-    assert all_events[-2:] == [
-        WorkerEvent(
-            state=WorkerState.IDLE,
-            task_status=TaskStatus(
-                task_id=task_id,
-                task_complete=False,
-                task_failed=False,
-            ),
-        ),
-        WorkerEvent(
-            state=WorkerState.IDLE,
-            task_status=TaskStatus(
-                task_id=task_id,
-                task_complete=True,
-                task_failed=False,
-            ),
-        ),
-    ]
-
-
 @pytest.mark.parametrize("device", ["sample_stage", "panda", "det"])
 def test_device_present(client: BlueapiClient, device: str):
     assert client.get_device(device), f"{device} is not available"
